c5855a23076782535247f606fbdb4b9b026b031c
Sergei Turchanov <turchanov@farpost.com>
2021-06-03 17:36:55 +1000

    Restart policy and and a memory limit for systemd service

--- scripts/gen-service.sh
+++ scripts/gen-service.sh
@@ -12,6 +12,11 @@ Description=Gluster Prometheus Exporter
 [Service]
 ExecStart=${SBINDIR}/gluster-exporter --config=${SYSCONFDIR}/gluster-exporter/gluster-exporter.toml
 KillMode=process
+StartLimitInterval=60
+StartLimitBurst=5
+Restart=on-failure
+RestartSec=10s
+MemoryLimit=100M
 
 [Install]
 WantedBy=multi-user.target
